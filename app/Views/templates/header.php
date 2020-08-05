31c029166595f9ee1f3fc1b8dcdaee22c7d1a4cb
Dev001 <pooja.sharma@cynoteck.com>
2020-08-05 19:53:54 +0530

    update header file

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -6,6 +6,9 @@
     <title></title>
     <!-- Bootstrap core CSS -->
     <?php 
+    ini_set('display_errors', '1');
+    ini_set('display_startup_errors', '1');
+    error_reporting(E_ALL);
 echo link_tag('assets/css/style.css');
 echo link_tag('assets/css/bootstrap.min.css');
 echo link_tag('assets/css/sticky-footer-navbar.css');
