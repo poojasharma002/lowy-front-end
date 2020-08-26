1e8cdd14cb14734c72fec2cca42872e39562cd6f
Shamim Ali <shamim.ali@cynoteck.com>
2020-08-26 17:38:32 +0530

    update backend api IP

--- app/Helpers/Globals_helper.php
+++ app/Helpers/Globals_helper.php
@@ -1,5 +1,5 @@
 <?php if(!function_exists('baseURI')) {
        function baseURI(){
-           return 'http://18.222.155.57/frameapp/';
+           return 'http://18.223.206.206/frameapp/';
        }
 }
