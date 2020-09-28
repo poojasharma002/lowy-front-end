80dc8a0addeb3ea6efe3f04e692411c8e32637ac
Shamim Ali <shamim.ali@cynoteck.com>
2020-09-28 16:03:20 +0530

    created json file code.

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -26,7 +26,7 @@ class BaseController extends Controller
 	 *
 	 * @var array
 	 */
-	protected $helpers = ['html','Globals_helper'];
+	protected $helpers = ['html','Globals_helper','filesystem'];
 
 	/**
 	 * Constructor.
--- app/Controllers/FrameList.php
+++ app/Controllers/FrameList.php
@@ -30,6 +30,47 @@ class FrameList extends BaseController
          }
        
      }
-	//--------------------------------------------------------------------
+   //--------------------------------------------------------------------
+     // function for create json data file  
+     public function json_create(){
+      try{
+         $baseURI = baseURI();
+         $client = \Config\Services::curlrequest();
+         //  $file = new \CodeIgniter\Files\File('result.json',false);
+                $response = $client->request('GET', ''.$baseURI.'frames?startRow=0&numRows=10000');
+                $result= $response->getBody();
+                $result = json_decode($result);
+              for($i=0; $i<count($result); $i++){
+               $numlength = strlen((string)$result[$i]->inventoryNumber);
+               if($numlength==1){ $invNo='L000'.$result[$i]->inventoryNumber;
+               }elseif($numlength==2){ $invNo='L00'.$result[$i]->inventoryNumber;
+               }elseif($numlength==3){ $invNo='L0'.$result[$i]->inventoryNumber; 
+               }elseif($numlength>=4){ $invNo='L'.$result[$i]->inventoryNumber;  }
+               $frames[] = array(
+                'inventoryNumber'  => "<a href ='".base_url()."/frameAdminView.action?id=".$result[$i]->inventoryNumber."'>".$invNo."</a>",
+                'description'      => $result[$i]->description,
+                'century'          => $result[$i]->century,
+                'countryCode'      => $result[$i]->countryCode,
+                'frameWidth'       => $result[$i]->frameWidth,
+                'sightHeight'      => $result[$i]->sightHeight,
+                'sightWidth'       => $result[$i]->sightWidth,
+                'purchaseDate'     => $result[$i]->purchaseDate,
+                'pairId'           => $result[$i]->pairId,
+                'buildingName'     => $result[$i]->buildingName,
+                'locationName'     => $result[$i]->locationName
+            );
+            } 
+      
+
+            $responseData['data'] = $frames;
+            write_file('./results.json',json_encode($responseData), 'w');
+            
+      } catch (Error $e){
+
+      }
+    
+  }
+//--------------------------------------------------------------------
+   
 
 }
