20056f665f52e1d9b2bc142db5cf732fa2336e3b
Shamim <pooja.sharma@cynoteck.com>
2020-10-13 16:31:27 +0530

    update base url

--- app/Config/App.php
+++ app/Config/App.php
@@ -21,7 +21,7 @@ class App extends BaseConfig
 	| environments.
 	|
 	*/
-	public $baseURL = 'http://localhost/lowy-front-end/';
+	public $baseURL = 'http://3.15.162.229/';
 
 	/*
 	|--------------------------------------------------------------------------
