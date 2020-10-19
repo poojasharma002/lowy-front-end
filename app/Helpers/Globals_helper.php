1b51a42e398966d2cbf363c0813b74e41169e3b6
poojasharma002 <68904263+poojasharma002@users.noreply.github.com>
2020-10-19 20:45:59 +0530

    Update Globals_helper.php

--- app/Helpers/Globals_helper.php
+++ app/Helpers/Globals_helper.php
@@ -1,5 +1,5 @@
 <?php if(!function_exists('baseURI')) {
        function baseURI(){
-           return 'http://3.138.68.13/frameapp/';
+           return 'http://3.138.178.129/frameapp/';
        }
 }
