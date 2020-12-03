569d9a1d95196a868cda5fcda163f5c5b6f4d854
Shamim <pooja.sharma@cynoteck.com>
2020-12-03 10:34:07 +0530

    change Spelling Fornt  to Front.

--- app/Views/login.php
+++ app/Views/login.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Lowy Fornt End Login</title>
+<title>Lowy Front End Login</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
