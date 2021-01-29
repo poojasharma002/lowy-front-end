0902ff47d8ce3ab49c06821c663aff4307ae5fd5
Shamim <pooja.sharma@cynoteck.com>
2021-01-29 10:33:49 +0530

    update Backend IP.

--- app/Helpers/Globals_helper.php
+++ app/Helpers/Globals_helper.php
@@ -1,5 +1,5 @@
 <?php if(!function_exists('baseURI')) {
        function baseURI(){
-           return 'http://3.139.74.184/frameapp/';
+           return 'http://3.15.138.122/frameapp/';
        }
 }
