afc037a7267585bff99b258db9a583b1e4272121
Shamim <pooja.sharma@cynoteck.com>
2021-03-04 10:12:02 +0530

    update domain name backend API.

--- app/Helpers/Globals_helper.php
+++ app/Helpers/Globals_helper.php
@@ -1,5 +1,5 @@
 <?php if(!function_exists('baseURI')) {
        function baseURI(){
-           return 'http://3.15.182.160/frameapp/';
+           return 'https://lowyfs.com/frameapp/';
        }
 }
